04a086b909777b01ca7f05028836266d1938dbdb
Carla Ramirez <ramirezcarla21@yahoo.com>
2019-12-11 08:01:32 -0600

    updated mailing list

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,7 +2,7 @@ class Visitor
   include ActiveModel::Model
   attr_accessor :email
   validates_presence_of :email
-  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/
+  validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
@@ -10,7 +10,7 @@ class Visitor
     result = mailchimp.lists(list_id).members.create(
       body: {
         email_address: self.email,
-	status: 'subscribed'
+        status: 'subscribed'
     })
     Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
